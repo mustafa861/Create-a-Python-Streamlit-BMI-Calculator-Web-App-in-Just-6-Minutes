b0db263687fbaf16c35979a0ad26d024566787c8
mustafa861 <mustafa1212adnan@gmail.com>
2025-03-12 00:18:49 +0500

    .

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(page_title="BMI Calculator", layout="wide")
-st.title("BMI Calculator")
-
-height = st.slider("Enter your height (in cm):", 100, 250, 175)
-weight = st.slider("Enter your weight (in cm):",40, 200, 70)
